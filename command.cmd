95a457a120c4b24fcda90b72bb1650d2585a94db
mustafoyevdeveloper <mustafoyevdevelopment.com>
2026-01-19 12:06:48 +0500

    automatic commented

--- command.cmd
+++ command.cmd
@@ -1,14 +1,11 @@
 # Frontend
-
 cd frontend && npm install && npm run dev
 cd frontend && npm run dev
 
 
 # Backend
-
 cd backend && npm install && npm run dev
 cd backend && npm start
 
 # Git push
-
 git add . && git commit -m "automatic commented" && git push
