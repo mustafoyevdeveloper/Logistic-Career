c86bb0b29bc7d9285cd0dd96c9b19be71f713d34
Jo'rabek Mustafoyev <mustafoyevdevelopment@gmail.com>
2026-02-22 10:18:39 +0500

    automatic commented

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-2ae722a1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.7l5vju48lrc"
+    "revision": "0.1dn1m35s3nc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
